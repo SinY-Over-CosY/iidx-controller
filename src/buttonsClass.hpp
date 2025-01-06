433ad124c035dc2495e7a8f2e6c539b5b70fa115
SinY/CosY <elitan.uk@gmail.com>
2025-01-06 16:47:16 +0000

    fixed redefinition issue in header file

--- src/buttonsClass.hpp
+++ src/buttonsClass.hpp
@@ -6,31 +6,17 @@ class Buttons{
         char buttons[7] = { 'z', 's', 'x', 'd', 'c', 'f', 'v' };
 
     public:
-        char getBut1(){
-            return buttons[0];
-        }
+        char getBut1();
 
-        char getBut2(){
-            return buttons[1];
-        }
+        char getBut2();
 
-        char getBut3(){
-            return buttons[2];
-        }
+        char getBut3();
 
-        char getBut4(){
-            return buttons[3];
-        }
+        char getBut4();
 
-        char getBut5(){
-            return buttons[4];
-        }
+        char getBut5();
 
-        char getBut6(){
-            return buttons[5];
-        }
+        char getBut6();
 
-        char getBut7(){
-            return buttons[6];
-        }
+        char getBut7();
 };
