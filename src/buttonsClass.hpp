a41a2ba0d0baaa52ed100ea9e35a8bd20b4f3913
SinY/CosY <elitan.uk@gmail.com>
2025-01-06 01:25:25 +0000

    added buttons class header file

---
+++ src/buttonsClass.hpp
@@ -0,0 +1,36 @@
+#include <Arduino.h>
+
+class Buttons{
+    private:
+        // usb protocol keyboard inputs for each button
+        char buttons[7] = { 'z', 's', 'x', 'd', 'c', 'f', 'v' };
+
+    public:
+        char getBut1(){
+            return buttons[0];
+        }
+
+        char getBut2(){
+            return buttons[1];
+        }
+
+        char getBut3(){
+            return buttons[2];
+        }
+
+        char getBut4(){
+            return buttons[3];
+        }
+
+        char getBut5(){
+            return buttons[4];
+        }
+
+        char getBut6(){
+            return buttons[5];
+        }
+
+        char getBut7(){
+            return buttons[6];
+        }
+};
