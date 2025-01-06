36b5735f0ff5eda465858ba49bcd58248e1220fe
SinY/CosY <elitan.uk@gmail.com>
2025-01-06 01:24:50 +0000

    added buttons class header files and getter methods

--- src/buttonsClass.cpp
+++ src/buttonsClass.cpp
@@ -1,30 +1,30 @@
 #include <Arduino.h>
+#include "buttonsClass.hpp"
 
-class Buttons{
-    private:
-        char buttons[7] = { 'z', 's', 'x', 'd', 'c', 'f', 'v'};
+char Buttons::getBut1(){
+    return buttons[0];
+}
 
-    public:
-        char getBut1(){
-            return buttons[0];
-        }
-        char getBut2(){
-            return buttons[1];
-        }
-        char getBut3(){
-            return buttons[2];
-        }
-        char getBut4(){
-            return buttons[3];
-        }
-        char getBut5(){
-            return buttons[4];
-        }
-        char getBut6(){
-            return buttons[5];
-        }
-        char getBut7(){
-            return buttons[6];
-        }
+char Buttons::getBut2(){
+    return buttons[1];
+}
 
-};
+char Buttons::getBut3(){
+    return buttons[2];
+}
+
+char Buttons::getBut4(){
+    return buttons[3];
+}
+
+char Buttons::getBut5(){
+    return buttons[4];
+}
+
+char Buttons::getBut6(){
+    return buttons[5];
+}
+
+char Buttons::getBut7(){
+    return buttons[6];
+}
